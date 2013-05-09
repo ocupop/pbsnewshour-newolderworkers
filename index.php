7eb900e8655576c2fee544f557e96c7655617f90
matthewmcvickar <matthew@matthewmcvickar.com>
2013-05-08 18:13:54 -1000

    Added in first page of content.

--- index.php
+++ index.php
@@ -1,11 +1,78 @@
 <?php require('header.php') ?>
 
+<!--
+
+Section 1: Rethinking Retirement
+
+-->
+
 <article class="rethinking_retirement">
 
-  <section class="">
-    
+  <section class="section1">
+    <p>The increase of workers 65 and older is due to a variety of factors&mdash;the implications of living longer chief among them.</p>
   </section>
 
+  <section class="section2">
+    <p>The percentage of workers who expect to keep working after age 65 has steadily increased in recent years.</p>
+    <p>From 11% in 1991 to 17% in 1997; 18% in 2002; 24% in 2007; and now 36% in 2013.</p>
+  </section>
+
+  <section class="section3">
+    <p>Nearly four in ten adults working past the median retirement age of 62 say it is because of the Great Recession.</p>
+  </section>
+
+  <section class="section4">
+    <p>The difference between what Americans have saved for retirement and what they need to maintain their current standard of living is $6.6 trillion.</p>
+    <p>Half of American households have less than $10,000 in savings.</p>
+  </section>
+
+  <section class="section5">
+    <p>Nearly one in four workers will be over the age of 55 by 2016. Why? Mainly because older Americans are working longer, and there are more of them. (The Baby Boom, remember.)</p>
+  </section>
+
+  <section class="section3">
+    <p>Everyone now knows that the workplace is greying. For a variety of reasons&mdash;the implications of living longer chief among them -- many of us plan to work longer and retire later than we would have just 10 years ago. But for how much longer? The answer has been changing rapidly. To help keep the answer up-to-date, we are asking you to weigh in.</p>
+  </section>
+
+  <section class="question expected_retirement_age">
+    <label>When do you think you&rsquo;ll retire?</label>
+    <ul>
+      <li>I want to retire before I reach age 65.</li>
+      <li>Between 65 and 69.</li>
+      <li>Between 70 and 74.</li>
+      <li>Between 75 and 79.</li>
+      <li>Past 80.</li>
+      <li>Never!</li>
+      <li>I am already retired.</li>
+    </ul>
+  </section>
+
+  <section class="question age">
+    <label>How old are you?</label>
+    <input type="range">
+    <div class="tables">
+    </div>
+  </section>
+
+  <section class="paul intro">
+    <p>At 68, I have now worked in journalism for 50 years (counting college). I have five grandchildren who demand, deserve and bounteously repay my attention. What hair I still have has been gray for years, and I feel the need to touch up my mustache with mascara. My fastball would be lucky to break 40 mph, but a cranky shoulder prevents me from even trying. And yet, I work two jobs&mdash;teaching my brand of real-world economics on the NewsHour (though technically, I&rsquo;m the show&rsquo;s economics reporter) and to college students, mainly at Yale but, occasionally, elsewhere.</p>
+
+    <p>I consider the jobs a privilege, regardless of age, consider myself &ldquo;absurdly fortunate&rdquo; (as my father once said in discussing my NewsHour job). So I intend to keep working as long as I possibly can.</p>
+
+    <p>As I pretty much knew, and have confirmed in the course of reporting for this older workers series, my situation is hardly typical of aging workers in America these days. And yet more Americans than ever, including more and more of my junior colleagues in the baby boom, are staying on the job past what was long considered retirement age. In twenty years workers 65&ndash;69 have doubled in their share of the labor force, and now almost a third of folks in that age range are working, up from one in five twenty years ago. What&rsquo;s more, the portion of people working past 75 has almost tripled since 1993.</p>
+
+    <div class="graph">
+      [Labor Force Participation Rates by Age Group, January 1993–January 2013 attachment. Most interesting: rate of change year over year for each age group. 75+ is dramatic]
+    </div>
+
+    <p>Is retirement as we know it becoming a thing of the past? How long are we likely to work and why? Will we be happy on the job, or miserable and counting the dollars and days until retirement? We&rsquo;ve spent much of the past year looking at the factors&mdash;demography, economics, and just plain personal preference&mdash;that help explain what&rsquo;s happening to the American workforce as it ages.</p>
+
+    <p>So what&rsquo;s going on? And what do statistics say about where you fit into the picture?</p>
+  </section>
+
+  <section class="video">
+    [TODO:PBS] [VIDEO: short video transition by Paul from this to the next section.]
+  </section>
 </article>
 
 <?php require('footer.php') ?>
