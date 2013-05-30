fe160153615a4293b9aa6d73fb731e21586a4d8c
matthewmcvickar <matthew@matthewmcvickar.com>
2013-05-30 03:42:57 -1000

    Simplify header styles.

--- chapter-5-moving-forward.php
+++ chapter-5-moving-forward.php
@@ -5,18 +5,33 @@ require_once('header.php');
 
 ?>
 
-<section class="intro">
+<section class="chapter_title">
+  <div class="background_image"></div>
+
   <div class="contents">
     <p>
-      The idea of working and retirement is changing.
+      Conclusion
     </p>
-    <p>How old are you?</p>
-    <input type="text" name="age">
-    <form id="the_form">
-      <input type="submit">
-    </form>
+    <h2>
+      Moving Forward
+    </h2>
+  </div>
+</section>
 
+<section class="share">
+  <div class="contents">
+    <h2>
+      Share what you&rsquo;ve learned
+      <span>Choose a title and image to share</span>
+    </h2>
 
+    <div class="share_options">
+      <ul>
+        <li>
+
+        </li>
+      </ul>
+    </div>
   </div>
 </section>
 
