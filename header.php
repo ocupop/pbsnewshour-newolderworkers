0f14eaa00d6514627aec72341079759577beb706
matthewmcvickar <matthew@matthewmcvickar.com>
2013-05-30 02:13:15 -1000

    No mobile version.

--- header.php
+++ header.php
@@ -23,7 +23,7 @@ $current_chapter = $chapter_list[$chapter];
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
   <title>PBS NewsHour: The Retiring of Retirement</title>
   <link rel="stylesheet" href="css/style.css">
   <script src="js/vendor/modernizr-2.6.2.min.js"></script>
