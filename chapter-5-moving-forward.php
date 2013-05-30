733bdbde4a13c4dc7abf7fdbc934670ac75595e5
matthewmcvickar <matthew@matthewmcvickar.com>
2013-05-30 05:40:06 -1000

    Chapter five.

--- chapter-5-moving-forward.php
+++ chapter-5-moving-forward.php
@@ -21,16 +21,145 @@ require_once('header.php');
 <section class="share">
   <div class="contents">
     <h2>
-      Share what you&rsquo;ve learned
-      <span>Choose a title and image to share</span>
+      Share What You&rsquo;ve Learned
+      <span>Choose a Title and Image to Share</span>
     </h2>
 
     <div class="share_options">
       <ul>
-        <li>
-
+        <li class="half_of_american_households">
+          <div class="services">
+            <span class="twitter">Twitter</span>
+            <span class="facebook">Facebook</span>
+            <span class="email">Email</span>
+          </div>
+          <img class="icon" src="img/5/share1.png">
+          <p>Half of American households have less than $10k in savings.</p>
+        </li>
+        <li class="death_of_the_pension">
+          <div class="services">
+            <span class="twitter">Twitter</span>
+            <span class="facebook">Facebook</span>
+            <span class="email">Email</span>
+          </div>
+          <img class="icon" src="img/5/share2.png">
+          <p>In 1975, 85% of private sector employees had pensions. In 2013, only 35% do.</p>
+        </li>
+        <li class="death_of_the_pension">
+          <div class="services">
+            <span class="twitter">Twitter</span>
+            <span class="facebook">Facebook</span>
+            <span class="email">Email</span>
+          </div>
+          <img class="icon" src="img/5/share3.png">
+          <p>The average investor underperforms during inflation.</p>
+        </li>
+        <li class="death_of_the_pension">
+          <div class="services">
+            <span class="twitter">Twitter</span>
+            <span class="facebook">Facebook</span>
+            <span class="email">Email</span>
+          </div>
+          <img class="icon" src="img/5/share4.png">
+          <p>A third of Baby Boomers have no retirement savings.</p>
         </li>
       </ul>
+    </div>
+  </div>
+</section>
+
+<section class="ask">
+  <div class="contents">
+    <h2>
+      Ask the Right Experts Questions
+    </h2>
+
+    <div class="ask_details">
+      <p class="first">
+        Curious how to maximize your own Social Security benefits, future or current? Submit questions to our retirement financial planning guru Larry Kotlikoff here. Then, watch these pages to see if he answers your question.<br>
+        <a href="TODO">TODO</a>
+      </p>
+      <p class="second">
+        If you&rsquo;re interested in job-hunting advice, whether you&rsquo;re a greenhorn or long in the tooth, submit your questions to &lsquo;Ask the Headhunter&rsquo; extraordinaire Nick Corcodilos. Watch these pages for advice and your questions answered.<br>
+        <a href="mailto:pbs@asktheheadhunter.com">Email Nick Corcodilos</a>
+      </p>
+    </div>
+  </div>
+</section>
+
+<section class="act">
+  <div class="contents">
+    <h2>
+      Act on These Three Steps Now
+      <span>Bootstrap Your Future</span>
+    </h2>
+
+    <div class="three_steps">
+      <div class="first_step">
+        <h3>
+          Don&rsquo;t forget to &ldquo;expect the unexpected&rdquo; and not underestimate your needs. These sites will help you get your finances in order.
+        </h3>
+        <ul>
+          <li>
+            <a href="http://www.choosetosave.org/ballpark/">Ballpark E$timate</a>
+            <p>
+              It is an easy-to-use, two-page worksheet that helps you quickly identify approximately how much you need to save to fund a comfortable retirement.
+            </p>
+          </li>
+          <li>
+            <a href="http://basic.esplanner.com/">ESPlanner Basic</a>
+            <p>
+              The free version of Larry Kotlikoff&rsquo;s retirement financial calculator.
+            </p>
+          </li>
+          <li>
+            <a href="http://www.aarp.org/work/social-security/social-security-benefits-calculator/?intcmp=SKYBOX2ENG">Social Security Calculator</a>
+            <p>
+              Find out when you should claim your benefits.
+            </p>
+          </li>
+        </ul>
+      </div>
+      <div class="second_step">
+        <h3>
+          See a financial advisor and arm yourself with these tips and information:
+        </h3>
+        <ul>
+          <li>
+            <a href="http://www.pbs.org/wgbh/pages/frontline/business-economy-financial-crisis/retirement-gamble/how-do-you-know-which-financial-adviser-to-trust/">How Do You Know Which Financial Adviser to Trust?</a>
+          </li>
+          <li>
+            <a href="http://www.pbs.org/wgbh/pages/frontline/business-economy-financial-crisis/retirement-gamble/what-to-ask-about-your-retirement/">What to Ask About Your Retirement</a>
+          </li>
+          <li>
+            <a href="http://www.pbs.org/wgbh/pages/frontline/business-economy-financial-crisis/retirement-gamble/how-retirement-fees-cost-you/">How Retirement Fees Cost You</a>
+          </li>
+        </ul>
+      </div>
+      <div class="third_step">
+        <h3>
+          Perhaps a second career is in your future, or working longer than the &ldquo;traditional&rdquo; retirement age?
+        </h3>
+
+        <p>
+          Today&rsquo;s older workers may very well need to have a plan in place to stay in the workforce longer. Start preparing when you are in your late 50s or early 60s for your next career, or work with your employer to adapt your job and add new skills as you get older. These are good places to start for useful advice and how-tos:
+        </p>
+
+        <ul>
+          <li>
+            <a href="http://www.encore.org/">Encore.org</a>
+            <p>
+              It is an easy-to-use, two-page worksheet that helps you quickly identify approximately how much you need to save to fund a comfortable retirement.
+            </p>
+          </li>
+          <li>
+            <a href="http://www.aarp.org/work/working-after-retirement">AARP&rsquo;s &lsquo;Recareering&rsquo; Site</a>
+            <p>
+              It is an easy-to-use, two-page worksheet that helps you quickly identify approximately how much you need to save to fund a comfortable retirement.
+            </p>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </section>
