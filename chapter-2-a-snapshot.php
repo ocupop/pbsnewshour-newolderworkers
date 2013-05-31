3ee88578bcd7438fbb92137c4ddfdf2314095904
matthewmcvickar <matthew@matthewmcvickar.com>
2013-05-30 21:15:47 -1000

    Male/female section. (Close #41)

--- chapter-2-a-snapshot.php
+++ chapter-2-a-snapshot.php
@@ -31,6 +31,8 @@ require_once('header.php');
 </section>
 
 <section class="question gender">
+  <div class="background_image"></div>
+
   <div class="contents">
     <p class="form_question">
       Are you male or female?
@@ -48,128 +50,124 @@ require_once('header.php');
       </label>
     </div>
 
-    <!-- TODO: show data: http://htmlpad.org/poll-gender/ -->
+    <div class="gender_split">
+      <section class="copy female">
+        <div class="contents">
+          <p>
+            You are likely to live longer and thus have higher health care expenses than men.
+          </p>
+          <p>
+            You are more likely to work in part-time jobs that don&rsquo;t qualify for a retirement plan.
+          </p>
+        </div>
+      </section>
+
+      <section class="copy male">
+        <div class="contents">
+          <p>
+            You are likely to have fewer health care expenses than women because on average men don&rsquo;t live as long as women.
+          </p>
+        </div>
+      </section>
+
+      <section class="quotation paula_gjerstad female">
+        <div class="contents">
+          <blockquote>
+            <p>
+              I started a new career at the age of 54 and I am now teaching nursing. I feel young, and luckily my body matches that life vision.
+            </p>
+            <cite>
+              <strong>&mdash; Paula Gjerstad, 68</strong>
+            </cite>
+          </blockquote>
+        </div>
+      </section>
+
+      <section class="copy male">
+        <div class="contents">
+          <p>
+            You are significantly more likely than a woman to feel very confident about having enough money for medical expenses and that you will live comfortably in retirement.
+          </p>
+        </div>
+      </section>
+
+      <section class="copy female">
+        <div class="contents">
+          <p>
+            In addition, working women are more likely than men to interrupt their careers to take care of family members, which means lower lifetime income, lower contributions to Social Security and thus lower Social Security benefits upon retirement.
+          </p>
+
+          <p>
+            You make 77 cents for every dollar a man makes, as an average across all occupations. So again, your Social Security benefits, should you qualify, will be a bit lower. Our Social Security guru, Larry Kotlikoff (more on him later) ran a simulation for a woman earning $77,000 a year and a man earning $100,000 a year for the same job. Assuming they both begin working at 30, retire at 65 and start taking benefits at 67, the man will receive an annual benefit of $39,020. The woman, $34,688&mdash;about 88% of the man’s benefit. So while women are still getting less than their higher-paid male counterparts, a bit of the difference is made up in annual SS benefits.
+          </p>
+
+          <p>
+            On average, your retirement account is worth 38.25% <strong>less</strong> than a man&rsquo;s. (However, that gap is slowly closing.)
+          </p>
+        </div>
+      </section>
+
+      <section class="copy male">
+        <div class="contents">
+          <p>
+            You&rsquo;re slightly less likely than a woman to participate in a retirement plan, when controlling for work status or earnings.
+          </p>
+        </div>
+      </section>
+
+      <section class="quotation jack_vanderhei female">
+        <div class="contents">
+          <blockquote>
+            <p>
+              In a household where the wife stayed home to raise the kids and then enters  the workforce in her 40s or 50s, you often find she will put a much, much higher percentage into her retirement savings than a woman of the same age who’s worked her whole life. This is an acknowledgement that she has to catch up for all those years she missed.
+            </p>
+            <cite>
+              <strong>&mdash; Jack VanDerhei</strong>
+              Research Director, Employee Benefit Research Institute
+            </cite>
+          </blockquote>
+        </div>
+      </section>
+
+      <section class="copy female">
+        <div class="contents">
+          <p>
+            You&rsquo;re slightly more likely than a man to participate in a retirement plan, after controlling for work status and earnings.
+          </p>
+          <p>
+            If you&rsquo;re a woman age 65 or older, you will likely be out of work for at least six months should you lose your job.
+          </p>
+        </div>
+      </section>
+
+      <section class="quotation michael_davidson male">
+        <div class="contents">
+          <blockquote>
+            <p>
+              I was tentatively flirting with early retirement in the late &lsquo;90s until my neatly-built nest egg disappeared with the dot-com downturn. I stayed on and continued to progress up the ladder of success until 2008 when that rug was pulled out from under me&hellip; again! I am now a farmer! And I'm going to stay a farmer until they pry my cold, dead hands off my (soon to be bought) tractor.
+            </p>
+            <cite>
+              <strong>&mdash; Michael Davidson, 65</strong>
+            </cite>
+          </blockquote>
+        </div>
+      </section>
+
+      <section class="quotation traer_sunley female">
+        <div class="contents">
+          <blockquote>
+            <p>
+              After I retired [at age 66] I competed for a volunteer docent position at a large local museum, received two years of intensive training comparable to an undergraduate degree in art history, and am now immersed in a whole new life of work! It is wonderful to re-pot yourself and let younger people step up.
+            </p>
+            <cite>
+              <strong>&mdash; Traer Sunley, 70</strong>
+            </cite>
+          </blockquote>
+        </div>
+      </section>
+    </div>
   </div>
 </section>
-
-<div class="gender_split">
-
-  <section class="copy female">
-    <div class="contents">
-      <p>
-        You are likely to live longer and thus have higher health care expenses than men.
-      </p>
-      <p>
-        You are more likely to work in part-time jobs that don&rsquo;t qualify for a retirement plan.
-      </p>
-    </div>
-  </section>
-
-  <section class="copy male">
-    <div class="contents">
-      <p>
-        You are likely to have fewer health care expenses than women because on average men don&rsquo;t live as long as women.
-      </p>
-    </div>
-  </section>
-
-  <section class="quotation paula_gjerstad female">
-    <div class="contents">
-      <blockquote>
-        <p>
-          I started a new career at the age of 54 and I am now teaching nursing. I feel young, and luckily my body matches that life vision.
-        </p>
-        <cite>
-          <strong>&mdash; Paula Gjerstad, 68</strong>
-        </cite>
-      </blockquote>
-    </div>
-  </section>
-
-  <section class="copy male">
-    <div class="contents">
-      <p>
-        You are significantly more likely than a woman to feel very confident about having enough money for medical expenses and that you will live comfortably in retirement.
-      </p>
-    </div>
-  </section>
-
-  <section class="copy female">
-    <div class="contents">
-      <p>
-        In addition, working women are more likely than men to interrupt their careers to take care of family members, which means lower lifetime income, lower contributions to Social Security and thus lower Social Security benefits upon retirement.
-      </p>
-
-      <p>
-        You make 77 cents for every dollar a man makes, as an average across all occupations. So again, your Social Security benefits, should you qualify, will be a bit lower. Our Social Security guru, Larry Kotlikoff (more on him later) ran a simulation for a woman earning $77,000 a year and a man earning $100,000 a year for the same job. Assuming they both begin working at 30, retire at 65 and start taking benefits at 67, the man will receive an annual benefit of $39,020. The woman, $34,688&mdash;about 88% of the man’s benefit. So while women are still getting less than their higher-paid male counterparts, a bit of the difference is made up in annual SS benefits.
-      </p>
-
-      <p>
-        On average, your retirement account is worth 38.25% <strong>less</strong> than a man&rsquo;s. (However, that gap is slowly closing.)
-      </p>
-    </div>
-  </section>
-
-  <section class="copy male">
-    <div class="contents">
-      <p>
-        You&rsquo;re slightly less likely than a woman to participate in a retirement plan, when controlling for work status or earnings.
-      </p>
-    </div>
-  </section>
-
-  <section class="quotation jack_vanderhei female">
-    <div class="contents">
-      <blockquote>
-        <p>
-          In a household where the wife stayed home to raise the kids and then enters  the workforce in her 40s or 50s, you often find she will put a much, much higher percentage into her retirement savings than a woman of the same age who’s worked her whole life. This is an acknowledgement that she has to catch up for all those years she missed.
-        </p>
-        <cite>
-          <strong>&mdash; Jack VanDerhei</strong>
-          Research Director, Employee Benefit Research Institute
-        </cite>
-      </blockquote>
-    </div>
-  </section>
-
-  <section class="copy female">
-    <div class="contents">
-      <p>
-        You&rsquo;re slightly more likely than a man to participate in a retirement plan, after controlling for work status and earnings.
-      </p>
-      <p>
-        If you&rsquo;re a woman age 65 or older, you will likely be out of work for at least six months should you lose your job.
-      </p>
-    </div>
-  </section>
-
-  <section class="quotation michael_davidson male">
-    <div class="contents">
-      <blockquote>
-        <p>
-          I was tentatively flirting with early retirement in the late &lsquo;90s until my neatly-built nest egg disappeared with the dot-com downturn. I stayed on and continued to progress up the ladder of success until 2008 when that rug was pulled out from under me&hellip; again! I am now a farmer! And I'm going to stay a farmer until they pry my cold, dead hands off my (soon to be bought) tractor.
-        </p>
-        <cite>
-          <strong>&mdash; Michael Davidson, 65</strong>
-        </cite>
-      </blockquote>
-    </div>
-  </section>
-
-  <section class="quotation traer_sunley female">
-    <div class="contents">
-      <blockquote>
-        <p>
-          After I retired [at age 66] I competed for a volunteer docent position at a large local museum, received two years of intensive training comparable to an undergraduate degree in art history, and am now immersed in a whole new life of work! It is wonderful to re-pot yourself and let younger people step up.
-        </p>
-        <cite>
-          <strong>&mdash; Traer Sunley, 70</strong>
-        </cite>
-      </blockquote>
-    </div>
-  </section>
-
-</div>
 
 <section class="copy men_and_women">
   <div class="background_image"></div>
